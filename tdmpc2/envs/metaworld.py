42e8e46b777ecdc6779bd506dacfd6c5a40fa2cb
MohamedGNabail <mohamed.nabail@robotics.utias.utoronto.ca>
2025-05-22 23:13:52 -0400

    Revert "Revert "draft for ubp2""
    This reverts commit 907a6a9ddf63b7c211a130b121c70048f42d8cc8.

--- tdmpc2/envs/metaworld.py
+++ tdmpc2/envs/metaworld.py
@@ -1,5 +1,5 @@
 import numpy as np
-import gym
+import gymnasium as gym
 from envs.wrappers.timeout import Timeout
 
 from metaworld.envs import ALL_V2_ENVIRONMENTS_GOAL_OBSERVABLE
@@ -15,14 +15,15 @@ class MetaWorldWrapper(gym.Wrapper):
 		self.env._freeze_rand_vec = False
 
 	def reset(self, **kwargs):
-		obs = super().reset(**kwargs).astype(np.float32)
+		obs , _ = super().reset(**kwargs)
+		obs = np.asarray(obs, dtype=np.float32)
 		self.env.step(np.zeros(self.env.action_space.shape))
 		return obs
 
 	def step(self, action):
 		reward = 0
 		for _ in range(2):
-			obs, r, _, info = self.env.step(action.copy())
+			obs, r, _, truncated ,info = self.env.step(action.copy())
 			reward += r
 		obs = obs.astype(np.float32)
 		return obs, reward, False, info
@@ -32,9 +33,7 @@ class MetaWorldWrapper(gym.Wrapper):
 		return self.env.unwrapped
 
 	def render(self, *args, **kwargs):
-		return self.env.render(
-			offscreen=True, resolution=(384, 384), camera_name=self.camera_name
-		).copy()
+		return self.env.render().copy()
 
 
 def make_env(cfg):
@@ -45,8 +44,9 @@ def make_env(cfg):
 	if not cfg.task.startswith('mw-') or env_id not in ALL_V2_ENVIRONMENTS_GOAL_OBSERVABLE:
 		raise ValueError('Unknown task:', cfg.task)
 	assert cfg.obs == 'state', 'This task only supports state observations.'
-	env = ALL_V2_ENVIRONMENTS_GOAL_OBSERVABLE[env_id](seed=cfg.seed)
+	env = ALL_V2_ENVIRONMENTS_GOAL_OBSERVABLE[env_id](seed=cfg.seed , render_mode="rgb_array")
 	env = MetaWorldWrapper(env, cfg)
 	env = Timeout(env, max_episode_steps=100)
-	env.max_episode_steps = env._max_episode_steps
+	assert (env.max_episode_steps == env._max_episode_steps), 'Meta-World envs should have max_episode_steps=env._max_episode_steps'
+	# env.max_episode_steps = env._max_episode_steps
 	return env
