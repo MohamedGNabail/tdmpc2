abafd79964c1d9b1e69881d2531892e61dfc85e4
MohamedGNabail <mohamed.nabail@robotics.utias.utoronto.ca>
2025-07-23 15:11:37 -0400

    temp offline wanbd

--- tdmpc2/common/logger.py
+++ tdmpc2/common/logger.py
@@ -139,7 +139,7 @@ class Logger:
 				],
 			dir=self._log_dir,
 			config=dataclasses.asdict(cfg),
-			mode="online"
+			mode="offline"
 		)
 		print(colored("Logs will be synced with wandb.", "blue", attrs=["bold"]))
 		self._wandb = wandb
